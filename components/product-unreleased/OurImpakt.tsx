27a471e7a3fb10167c6239937bb92c5ca631baa6
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2026-05-07 16:12:32 +0500

    update

--- components/product-unreleased/OurImpakt.tsx
+++ components/product-unreleased/OurImpakt.tsx
@@ -33,7 +33,7 @@ const OurImpakt = () => {
   return (
     <section className="w-full py-10 lg:py-20">
       <div className="mx-auto max-w-7xl px-4 lg:px-6">
-        <div className="flex flex-col items-center text-center gap-6 pb-8 lg:pb-0">
+        <div className="flex flex-col items-center text-center gap-6 pb-8 lg:pb-12">
           <h2 className="text-3xl lg:text-5xl max-w-3xl font-medium leading-tight text-gray-900">
             Our Impact in Numbers
           </h2>
@@ -43,7 +43,7 @@ const OurImpakt = () => {
             through our expertise and innovative solutions.
           </p>
 
-          <div className="grid grid-cols-2 w-full lg:flex gap-3">
+          <div className="grid grid-cols-2 w-full lg:flex justify-center gap-3">
             <Button 
               className="bg-[#2563EB] hover:bg-[#2563EB]/90 h-10 w-full lg:w-[83px] rounded-[12px] shadow-[0px_1px_2px_0px_rgba(20,21,26,0.05)] opacity-100 text-xs font-medium text-white cursor-pointer"
               onClick={openDemoModal}
